596fb5adabab87614e7e16662e57390ba717be0c
Dhi Aurrahman <dio@rockybars.com>
2022-03-01 18:21:37 +0700

    Always true when missing ref
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- index.js
+++ index.js
@@ -5,47 +5,42 @@ const minimatch = require('minimatch');
 
 (async () => {
   const ghToken = core.getInput('github_token');
-  core.setOutput('modified', true);
-  // const octokit = new Octokit(ghToken ? { auth: ghToken } : {});
-  // const paths = core.getInput('paths').split(',');
+  const octokit = new Octokit(ghToken ? { auth: ghToken } : {});
+  const paths = core.getInput('paths').split(',');
 
-  // // When using pull_request, the context payload.head_commit is undefined. But we have after instead.
-  // let ref, owner, repo;
+  const returnFiles = core.getInput('return_files') === 'true';
+  // When using pull_request, the context payload.head_commit is undefined. But we have after instead.
+  const ref = github.context.payload.head_commit ?
+    github.context.payload.head_commit.id : github.context.payload.after;
 
-  // if (github.context.payload.head_commit) {
-  //   ref = github.context.payload.head_commit.id;
-  //   const parts = github.context.payload.repository.full_name.split('/');
-  //   owner = parts[0];
-  //   repo = parts[1];
-  // }
+  core.info(JSON.stringify(github.context.payload, null, 2));
 
-  // if (github.context.payload.pull_request) {
-  //   ref = github.context.payload.pull_request.head.sha;
-  //   const parts = github.context.payload.pull_request.head.repo.full_name.split('/');
-  //   owner = parts[0];
-  //   repo = parts[1];
-  // }
+  if (!ref) {
+    core.info('missing ref!');
+    core.setOutput('modified', true);
+    return;
+  }
 
-  // if (!ref) {
-  //   core.info(JSON.stringify(github.context.payload, null, 2));
-  //   core.setOutput('modified', true);
-  //   return
-  // }
+  const [owner, repo] = github.context.payload.repository.full_name.split('/');
+  const { data: { files }, err } = await octokit.repos.getCommit({ owner, repo, ref });
+  if (err) {
+    core.error(err);
+    core.setOutput('modified', true);
+    return;
+  }
 
-  // const { data: { files }, err } = await octokit.repos.getCommit({ owner, repo, ref })
-  // if (err) {
-  //   core.error(err);
-  //   core.setOutput('modified', true);
-  //   return
-  // }
+  if (Array.isArray(files)) {
+    const modifiedPaths = files.map(f => f.filename);
+    if (!returnFiles) {
+      const modified = paths.some(p => minimatch.match(modifiedPaths, p).length);
+      core.setOutput('modified', modified);
+      return;
+    }
+    const modified_files = paths.flatMap(p => minimatch.match(modifiedPaths, p));
+    core.setOutput('modified', modified_files.length > 0);
+    core.setOutput('modified_files', modified_files);
+    return;
+  }
 
-  // if (Array.isArray(files)) {
-  //   const modifiedPaths = files.map(f => f.filename);
-  //   core.info(JSON.stringify(modifiedPaths, null, 2));
-  //   const modified = paths.some(p => minimatch.match(modifiedPaths, p).length);
-  //   core.setOutput('modified', modified);
-  //   return
-  // }
-
-  // core.setOutput('modified', false);
+  core.setOutput('modified', false);
 })();
